3882ebee34f03cce97f2698645ae3641bd1dcca4
Vova Yatsyuk <vova.yatsyuk@gmail.com>
2024-07-03 09:57:33 +0300

    Unfinished gift wrapping integration

---
+++ view/frontend/web/js/gift-wrapping/gift-wrapping-deps.js
@@ -0,0 +1,8 @@
+define([
+    'jquery'
+], function ($) {
+    'use strict';
+
+    $.breezemap.__register('Magento_GiftWrapping/js/model/gift-wrapping', 0);
+    $.breezemap.__register('Magento_GiftWrapping/js/model/gift-wrapping-collection', 1);
+});
---
+++ view/frontend/web/js/gift-wrapping/gift-wrapping-view.js
@@ -0,0 +1,7 @@
+define([
+    'jquery'
+], function ($) {
+    'use strict';
+
+    $.breezemap.__register('Magento_GiftWrapping/js/view/gift-wrapping');
+});
